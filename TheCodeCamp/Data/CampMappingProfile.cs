88c16ab592bf9f1b684247feb5564bd1e4bd81e8
Yaseen Saleem <yaseen.saleem@trapezegroup.com>
2020-07-11 23:47:54 +0500

    The Code Camp Web API Project 07-11-2020

---
+++ TheCodeCamp/Data/CampMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Data
+{
+    public class CampMappingProfile : Profile
+    {
+        public CampMappingProfile()
+        {
+            CreateMap<Camp, CampModel>()
+              .ForMember(c => c.Venue, opt => opt.MapFrom(m => m.Location.VenueName))
+              .ReverseMap(); // in post we are doing campmodel to camp mapping so use reverse map
+
+        CreateMap<Talk, TalkModel>()
+        .ReverseMap()
+        .ForMember(t => t.Speaker, opt => opt.Ignore()) // when updating a talk you can ignore its sub objects talks and camps
+        .ForMember(t => t.Camp, opt => opt.Ignore());
+
+            CreateMap<Speaker, SpeakerModel>()
+                .ReverseMap();
+        }
+    }
+}
